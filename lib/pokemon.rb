d03fcdbc1de5e80fa2d2173d2360ed0e48f66401
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-20 23:39:27 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,6 +2,14 @@ class Pokemon
   attr_reader :id, :name, :type, :hp, :db
   @@all = []
 
+  def initialize (id:, name:, type:, hp: nil, db:)
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp
+    @db = db
+    @@all << self
+  end
 
   def self.save(name, type, database_connection)
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
@@ -19,4 +27,5 @@ class Pokemon
   def alter_hp(new_hp, database_connection)
     database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
   end
+
 end
